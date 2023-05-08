a9866294928888a031261e4ca6110d8dd01edee9
KnoxMLG <destroyernv2004@gmail.com>
2023-05-08 01:16:53 -0400

    Update views.py

--- metaabusers/metaabusers/views.py
+++ metaabusers/metaabusers/views.py
@@ -3,12 +3,14 @@ from django.http import HttpResponse
 from django.shortcuts import redirect
 from topplayers.withriotapi import getChallengerPlayers
 from . import rebestplayer
+from django.core.management import call_command
 
 def index(request):
     if request.method == 'POST':
         summoner_name = request.POST['inp_number']
         #players:player refers to topplayers/urls.py where app_name=players
         return redirect('players:player',player=summoner_name)
+    call_command('crontab add')
     return render(request, 'index.html')
 
 def topplayers(request):
