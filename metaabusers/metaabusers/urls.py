6b32f561a59d78eb86958a7f448cb0b007881cac
KnoxMLG <destroyernv2004@gmail.com>
2023-05-08 17:58:37 -0400

    Update urls.py

--- metaabusers/metaabusers/urls.py
+++ metaabusers/metaabusers/urls.py
@@ -16,7 +16,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from . import views, settings
-import static
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('', views.index, name='index'),
@@ -24,5 +24,4 @@ urlpatterns = [
     path('players/', include('topplayers.urls')),
     path('admin/', admin.site.urls),
 ]
-urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
