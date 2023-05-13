6755e5d7f0e54a4b57bcd9bac63fb057df509360
KnoxMLG <destroyernv2004@gmail.com>
2023-05-13 01:23:35 -0400

    fix multithreaded loads

--- metaabusers/tes.py
+++ metaabusers/tes.py
@@ -1,5 +1,5 @@
 from . import rebestplayer
-import threading
+
 
 def calculateBestPlayers():
     BestPlayers = list(rebestplayer.getPlayers().objects.values_list('autoinc', 'name', 'LP'))
@@ -14,32 +14,4 @@ def calculateBestPlayers():
     # Pass the result to the callback function
     return zipPlayers
 
-def swa():
-    is_running = threading.Event()
 
-    # Define a container for the result
-    result_container = []
-
-    def threaded_operation():
-        result = calculateBestPlayers()
-        result_container.append(result)
-        is_running.clear()  # Set the flag to indicate that the operation has completed
-
-    # Create a new thread and assign the threaded_operation function as the target
-    t = threading.Thread(target=threaded_operation)
-    t.start()
-
-    # Check if the threaded operation is still running
-    while is_running.is_set():
-        # The operation is running, perform actions accordingly
-        print("Threaded operation is still in progress...")
-        # You can perform additional actions while waiting for the operation to complete
-        t.join(timeout=1)  # Wait for 1 second before checking the status again
-
-    # Wait for the threaded operation to complete
-    t.join()
-
-    # Retrieve the result from the result container
-    result = result_container[0] if result_container else None
-
-    return result
--- metaabusers/views.py
+++ metaabusers/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.shortcuts import redirect
 from . import rebestplayer, tes
+import threading
 
 
 def index(request):
@@ -12,14 +13,42 @@ def index(request):
     return render(request, 'index.html')
 
 def topplayers(request):
-    print('hi')
     if request.method == 'POST':
         if request.POST.get("Refresh"):
-            rebestplayer.cron_reloadbest()
-    playerlist = tes.swa()
-    print('klwdnglk')
-    return HttpResponse(playerlist)
-    #return render(request, 'bestplayers.html')
+            result_container = []
+
+            def threaded_operation():
+                result = rebestplayer.cron_reloadbest()
+                result_container.append(result)
+                is_running.clear()  # Set the flag to indicate that the operation has completed
+
+            # Create a new thread and assign the threaded_operation function as the target
+            t = threading.Thread(target=threaded_operation, daemon=True)
+            t.start()
+            # Check if the threaded operation is still running
+    is_running = threading.Event()
+
+    # Define a container for the result
+    result_container = []
+
+    def threaded_operation():
+        result = tes.calculateBestPlayers()
+        result_container.append(result)
+        is_running.clear()  # Set the flag to indicate that the operation has completed
+
+    # Create a new thread and assign the threaded_operation function as the target
+    t = threading.Thread(target=threaded_operation, daemon=True)
+    t.start()
+
+    # Check if the threaded operation is still running
+
+    # Wait for the threaded operation to complete
+    t.join()
+
+    # Retrieve the result from the result container
+    result = result_container[0] if result_container else None
+
+    return render(request, 'bestplayers.html')
 
 
 
